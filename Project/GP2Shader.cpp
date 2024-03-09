07930a0e4b88f2b98edf164c8dd27501cdbf085c
Juddy2403 <119872018+Juddy2403@users.noreply.github.com>
2024-03-09 17:39:36 +0100

    better

--- Project/GP2Shader.cpp
+++ Project/GP2Shader.cpp
@@ -45,9 +45,19 @@ VkPipelineShaderStageCreateInfo GP2Shader::createVertexShaderInfo(const VkDevice
 VkPipelineVertexInputStateCreateInfo GP2Shader::createVertexInputStateInfo()
 {
 	VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+
+	auto bindingDescription = Vertex::getBindingDescription();
+	auto attributeDescriptions = Vertex::getAttributeDescriptions();
+
+	vertexInputInfo.vertexBindingDescriptionCount = 1;
+	vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
+	vertexInputInfo.pVertexBindingDescriptions = &bindingDescription;
+	vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
+
 	vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-	vertexInputInfo.vertexBindingDescriptionCount = 0; //&m_VertexInputBindingDescription
-	vertexInputInfo.vertexAttributeDescriptionCount = 0;
+	//vertexInputInfo.vertexBindingDescriptionCount = 0; //&m_VertexInputBindingDescription
+	//vertexInputInfo.vertexAttributeDescriptionCount = 0;
+
 	return vertexInputInfo;
 }
 
